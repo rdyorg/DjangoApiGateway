7ebffca53b9902fcff59aa6110528cc6e64cfea8
renjing <renjing@iotsh.com.cn>
2021-06-21 15:58:08 +0800

    resp结果json格式简化和树形结构组装

--- gateway/tests.py
+++ gateway/tests.py
@@ -1,21 +1,84 @@
-import asyncio
-from aiohttp import ClientSession
-import aiohttp
+a = {
+    "data": {
+        "aggResults": {
+            "openTS.day": [
+                {
+                    "count": 1,
+                    "time_fm": "2021-06-07"
+                },
+                {
+                    "count": 1,
+                    "time_fm": "2021-06-08"
+                },
+                {
+                    "count": 1,
+                    "time_fm": "2021-06-16"
+                },
+                {
+                    "count": 1,
+                    "time_fm": "2021-06-17"
+                }
+            ]
+        },
+        "total": 4
+    },
+    "result": "success"
+}
 
-urls = ['http://39.103.236.234:10003/', 'http://39.103.236.234:10003/']
+
+# 简化结果
+def easy_resp(a):
+    for key, i in a.items():
+        if isinstance(i, list):
+            a[key] = [i[0]] if len(i) else []
+            continue
+        if isinstance(i, dict):
+            easy_resp(i)
+    return a
 
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+# 组装结果
+def format_resp(index, a):
+    res = []
+    for key, i in a.items():
+        now_res = dict(
+            id=index,
+            label="{}  --  {}  --  {}".format(key, index, key),
+        )
+        index += 1
+        if isinstance(i, dict):
+            index, now_res["children"] = format_resp(index, i)
+        if isinstance(i, list):
+            if len(i):
+                index, now_res["children"] = format_resp(index, i[0])
+        res.append(now_res)
+    return index, res
 
+def main(a):
+    c = easy_resp(a)
+    print(c)
+    print(format_resp(1, c)[1])
 
-async def get(url):
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, url)
-        print(html)
+b = {
+    "data": {
+        "numHits": 4,
+        "messages": [],
+        "aggResults": {
+            "simple.messageType": [
+                {
+                    "count": 3,
+                    "messageType": "ProtectiveBuildingShakeDetection"
+                },
+                {
+                    "count": 1,
+                    "messageType": "inclinometer"
+                }
+            ]
+        }
+    },
+    "result": "success"
+}
 
-
-tasks = [get(x) for x in urls]
-loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(*tasks))
+if __name__ == '__main__':
+    main(a)
+    main(b)
