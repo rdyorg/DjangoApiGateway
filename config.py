7c04cf1f0121277c13c87963b351788506bb520c
Jamie Ren <37316282+RunningFaster@users.noreply.github.com>
2021-07-29 09:54:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,1 @@
-# ******项目运行时需要的配置信息*******
-# Django ENV
-DEBUG = True
-# Mysql连接
-MYSQL_HOST = "39.103.236.234"
-MYSQL_PORT = 3306
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "RJloveLD1314"
-MYSQL_DB = "api_gateway"
-# Redis连接
-REDIS_HOST = "39.103.236.234"
-REDIS_PORT = 6379
-REDIS_PASSWORD = "123456"
-REDIS_DB = 1
-# URL代理地址
-PROXY_PATH = "proxy/"
-TEST_PROXY_PATH = "_proxy/"
+
